afd3e49cb683b7993750c826fe189ac6a277cf35
Wcrafting <iam3ji0@gmail.com>
2020-01-31 21:09:03 +0300

    begin

---
+++ src/component/Loader/Loader.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Loader = () => (<div><h1>rere</h1></div> )
+
+export default Loader
--- src/container/Alco.js
+++ src/container/Alco.js
@@ -1,19 +1,30 @@
 import React,{useState, useEffect} from 'react'
+import Loader from '../component/Loader/Loader'
 const toJSON = response => response.json()
 
 const Alco = () => {
+    const [random, setRandom] = useState({})
+    const [loading, setLoading] = useState(false)
     useEffect(()=>{
-        getData()
+        getRandomCoctail()
     },[])
-    const getData = async () => {
+    const getRandomCoctail = async () => {
+        setLoading(true)
         const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
                         .then(toJSON)
 
         console.log(response.drinks[0])
+        setRandom(response.drinks[0])
+        setLoading(false)
+    }
+    console.log(random);
+    if(loading){
+        return <Loader/>
     }
     return(
-        <div>
-                sad
+        <div>   
+                <h2>{random.strDrink}</h2>
+                <img src={random.strDrinkThumb} alt=""/>
         </div>
     )
 }
