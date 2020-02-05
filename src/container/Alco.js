f4c3366ff46fca065ec232408e90f5ca289b2334
apekhnik <iam3ji0@gmail.com>
2020-02-05 14:36:30 +0300

    g

--- src/container/Alco.js
+++ src/container/Alco.js
@@ -12,19 +12,23 @@ const Alco = () => {
     const [ingredientList, setIngredientLis] = useState([])
     const [ingredientSearhcResult, setIngredientSearhcResult] = useState([])
     const [searchIngredientInput, setSearchIngredientInput] = useState('')
+    const [searchCoctailInput, setSearchCoctailInput] = useState('')
+    
+    
+    
     console.log(ingredientSearhcResult, 'search ingridient')
     console.log(ingredientList, 'list of ingredient')
     useEffect(()=>{
         getRandomCoctail()
         getIngredientList()
-        searchBy()
+        searchIngredient()
     },[])
     const getIngredientList = async () => {
         const ingList =  await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
                         .then(toJSON)
         setIngredientLis(ingList.drinks)
     }
-    const searchBy = async (search='gin') => {
+    const searchIngredient = async (search='gin') => {
         const searchResult = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${search}`)
                             .then(toJSON)
         setIngredientSearhcResult(searchResult.ingredients[0])
@@ -33,18 +37,18 @@ const Alco = () => {
         setLoading(true)
         const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
                         .then(toJSON)
-        const ingredientList = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?i=gin')
-                        .then(toJSON)
-        const response3 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass')
-                        .then(toJSON)
-        const glassList = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
-                        .then(toJSON)
-        const responseId = await fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007')
-                        .then(toJSON)
-        console.log(responseId, 'byID');
-        console.log(ingredientList, 'List of ingredient')
-        console.log(response3, 'Cocktail_glass')
-        console.log(glassList, 'List of glass');
+        // const ingredientList = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?i=gin')
+        //                 .then(toJSON)
+        // const response3 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass')
+        //                 .then(toJSON)
+        // const glassList = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+        //                 .then(toJSON)
+        // const responseId = await fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007')
+        //                 .then(toJSON)
+        // console.log(responseId, 'byID');
+        // console.log(ingredientList, 'List of ingredient')
+        // console.log(response3, 'Cocktail_glass')
+        // console.log(glassList, 'List of glass');
         console.log(response)
         setRandom(response.drinks[0])
         setTimeout(()=>{setLoading(false)}, 1500)
@@ -61,16 +65,28 @@ const Alco = () => {
     })
     return ingridient
     }
-    const searchHandler = ({ key }) => {
+    const searchIngredientHandler = ({ key }) => {
         if (key === 'Enter') {
-            searchBy(searchIngredientInput)
+            searchIngredient(searchIngredientInput)
         }
     }
     const ingredientInputChangeHandler  = (event) => {
         setSearchIngredientInput(event.target.value)
     }
     const coctailInputChangeHandler = e => {
-
+        setSearchCoctailInput(e.target.value)
+        console.log(searchCoctailInput)
+    }
+    const searchCoctailHandler = ({ key }) => {
+        if (key === 'Enter') {
+            searchCoctail()
+        }
+    }
+    const searchCoctail = async () =>{
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+                                .then(toJSON)
+        console.log(response.drinks[0])
+        setRandom(response.drinks[0])
     }
     const ingridient = getIngredient(random)
 
@@ -82,6 +98,15 @@ const Alco = () => {
     return(
         <div className="aplication">
                 <ContainerComp>
+                        <ContainerItemComp>
+                                <div className="listing">
+                                        <input type="text" 
+                                            onChange={coctailInputChangeHandler}
+                                            onKeyPress={searchCoctailHandler}
+                                        />
+                                        
+                                </div>
+                        </ContainerItemComp>
                         <ContainerItemComp>
                                 <Coctail
                                     src={random.strDrinkThumb}
@@ -98,14 +123,16 @@ const Alco = () => {
                                 />
                         </ContainerItemComp>
                         <ContainerItemComp>
-                                <input type="text" 
-                                onChange={ingredientInputChangeHandler}
-                                onKeyPress={searchHandler}
-                                />
-                                {ingredientList.map((item)=>{
-                                return <p>{item.strIngredient1}</p>
-                                    
-                                })}
+                                <div className="ingredient-listing">
+                                        <input type="text" 
+                                        onChange={ingredientInputChangeHandler}
+                                        onKeyPress={searchIngredientHandler}
+                                        />
+                                        {ingredientList.map((item)=>{
+                                        return <p onClick={()=>{searchIngredient(item.strIngredient1)}}>{item.strIngredient1}</p>
+                                            
+                                        })}
+                                </div>
                         </ContainerItemComp>
                 </ContainerComp>
         </div>
